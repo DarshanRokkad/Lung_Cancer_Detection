febba0e35d94bd6d16576373ac15f7793d9ddf0a
Darshan R.M <=>
2024-04-20 23:48:10 +0530

    adding training pipeline 2nd compo

--- src/pipeline/stage_02_prepare_base_model.py
+++ src/pipeline/stage_02_prepare_base_model.py
@@ -22,8 +22,11 @@ if __name__ == '__main__':
     try:
         logger.info(f"*******************")
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = PrepareBaseModelTrainingPipeline()
-        obj.initiate_prepare_base_model()
+        config = ConfigurationManager()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
