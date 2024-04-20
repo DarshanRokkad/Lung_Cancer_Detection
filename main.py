7cc122b0bf37509ab169e41ff898abaaa49d6102
Darshan R.M <=>
2024-04-20 18:56:19 +0530

    data ingestion tested

---
+++ main.py
@@ -0,0 +1,14 @@
+from src import logger
+from src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.initiate_data_ingestion()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
