3bf476a231d52b998504327f1adadbb041a537bf
Darshan R.M <=>
2024-04-20 15:34:24 +0530

    logging added

---
+++ src/__init__.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import logging
+
+
+log_dir = "logs"
+os.makedirs(os.getcwd(), log_dir, exist_ok=True)
+log_filepath = os.path.join(log_dir,"running_logs.log")
+
+logging.basicConfig(
+    level= logging.INFO,
+    format= "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]",
+    handlers=[
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger("CnnClassifierLogger")
