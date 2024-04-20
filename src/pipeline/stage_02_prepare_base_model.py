c13dfeec52bd5b6415fdf7fceedc7ecd840f652c
Darshan R.M <=>
2024-04-20 23:41:16 +0530

    training pipeline

---
+++ src/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,30 @@
+from src.config.configuration import ConfigurationManager
+from src.components.prepare_base_model import PrepareBaseModel
+from src import logger
+
+
+STAGE_NAME = 'Prepare Base Model'
+
+class PrepareBaseModelTrainingPipeline:
+    def __init__(self):
+        pass
+    
+    
+    def initiate_prepare_base_model(self):
+        config = ConfigurationManager()
+        prepare_base_model_config = config.get_data_ingestion_config()
+        prepare_base_model = PrepareBaseModel(config = prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
+        
+        
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.initiate_prepare_base_model()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
