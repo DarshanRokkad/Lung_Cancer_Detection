2ac520cf0cff3494edfddbdefa6becc326d17466
Darshan R.M <=>
2024-04-20 23:59:16 +0530

    fixed error

--- src/pipeline/stage_02_prepare_base_model.py
+++ src/pipeline/stage_02_prepare_base_model.py
@@ -12,7 +12,7 @@ class PrepareBaseModelTrainingPipeline:
     
     def initiate_prepare_base_model(self):
         config = ConfigurationManager()
-        prepare_base_model_config = config.get_data_ingestion_config()
+        prepare_base_model_config = config.get_prepare_base_model_config()
         prepare_base_model = PrepareBaseModel(config = prepare_base_model_config)
         prepare_base_model.get_base_model()
         prepare_base_model.update_base_model()
@@ -22,11 +22,8 @@ if __name__ == '__main__':
     try:
         logger.info(f"*******************")
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        config = ConfigurationManager()
-        prepare_base_model_config = config.get_prepare_base_model_config()
-        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
-        prepare_base_model.get_base_model()
-        prepare_base_model.update_base_model()
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.initiate_prepare_base_model()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
