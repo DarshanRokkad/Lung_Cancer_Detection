fc6e82653b59e80c8e8ed7f4db71aa68064f8b2e
Darshan R.M <=>
2024-04-22 10:27:57 +0530

    updated streamlit app

--- app.py
+++ app.py
@@ -4,17 +4,22 @@ from chest_cancer_classifier.pipeline.prediction_pipeline import PredictionPipel
 
 
 st.markdown("<h1 style='text-align: center;'>Chest Cancer Prediction</h1>", unsafe_allow_html=True)
+st.write("")
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+st.write("")
+st.write("")
 
 if uploaded_file is not None:
+    
     image = Image.open(uploaded_file)
     image_name = "uploaded_image.jpg"
     with open(image_name, "wb") as f:
         f.write(uploaded_file.getvalue())
     st.image(image, use_column_width="auto")
-
-    if st.button("Predict"):
+    st.write("")
+    
+    if st.button("Predict", type = "primary"):
         classifier = PredictionPipeline(image_name)
         prediction = classifier.predict()
         st.markdown(f"<h3 style='text-align: center;'>Prediction : {prediction}</h3>", unsafe_allow_html=True)
